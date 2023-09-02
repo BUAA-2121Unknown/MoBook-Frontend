edab0bbf6fe90de93eb695fbd91fd84e4a3556d0
gahow <89721635@qq.com>
2023-09-02 21:08:31 +0800

    beautify file tree

--- src/components/fileManager/FileTree.vue
+++ src/components/fileManager/FileTree.vue
@@ -28,6 +28,8 @@
           <!-- 编辑文件名 -->
           <span v-if="data.isInputVisible" class="file-tree-node">
             <el-input
+              class="input"
+              textareaStyle="backgound-color: #eeeeee;"
               v-model="editingName"
               ref="editingRef"
               @blur="handleEditComplete(data, editingName)"
@@ -540,6 +542,11 @@ const fileTreeList = [
     justify-content: space-between;
     font-size: 14px;
     padding-right: 8px;
+    height: 28px;
+
+    .input {
+      height: 22px;
+    }
   }
 
   .icon-wrapepr {
